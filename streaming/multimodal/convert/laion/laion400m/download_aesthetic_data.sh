a17308701ee5068bed4c57a268dedb59bf9ba238
Landan Seguin <landanjs@gmail.com>
2023-03-07 16:44:39 -0800

    Aesthetic data download script

---
+++ streaming/multimodal/convert/laion/laion400m/download_aesthetic_data.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if hash wandb 2> /dev/null; then
+    wandb login
+    ENABLE_WANDB=True
+else
+    ENABLE_WANDB=False
+fi
+
+img2dataset \
+    --url_list /tmp/laion2b-raw \
+    --input_format parquet \
+    --url_col URL \
+    --caption_col TEXT \
+    --output_format parquet \
+    --output_folder /tmp/laion2b-processed \
+    --processes_count 48 \
+    --thread_count 96 \
+    --resize_mode no \
+    --compute_hash "md5" \
+    --verify_hash '["md5","md5"]' \
+    --save_additional_columns '["punsafe","pwatermark","similarity","hash"]' \
+    --enable_wandb True
+
+touch /tmp/laion2b-processed/done
